f2001a706d581dd3270189195475a4013cc52463
TERRY SILVER <salutestoker@gmail.com>
2026-05-22 18:48:14 -0400

    adjusted table max widths

--- resources/js/Pages/Playbooks/Index.tsx
+++ resources/js/Pages/Playbooks/Index.tsx
@@ -24,7 +24,7 @@ export default function PlaybooksIndex({
                         alt=""
                     />
                 </div>
-                <div className="mx-auto max-w-7xl">
+                <div className="mx-auto max-w-[1500px]">
                     <Eyebrow>Playbook Access</Eyebrow>
                     <GradientHeading className="text-center">
                         Playbook Matrix
